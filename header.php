bcd599fdb2a1a1863df7dd7725e57fd34e33e4ef
Tanakit Sachati <bank21235@Tanakits-MacBook-Pro.local>
2018-06-06 21:58:58 +0900

    web tagger

---
+++ header.php
@@ -0,0 +1,30 @@
+
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.0.13/css/all.css" integrity="sha384-DNOHZ68U8hZfKXOrtjWvjxusGo9WQnrNx2sqG0tfsghAvtVlRW3tvkXWZh58N9jp" crossorigin="anonymous">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <script src="./bootstrap/js/bootstrap.min"></script>
+    <link rel="stylesheet" href="./bootstrap/css/bootstrap.min.css">
+    <link rel="stylesheet" href="./css/mystyles.css">
+    <title>TaggerEvo</title>
+</head>
+<body>
+    <head>
+        <nav class="navbar navbar-light bg-light ">
+            <div class="container">
+                <a class="navbar-brand" href="index.php">
+                <i class="fas fa-pen-square"></i>
+                TaggerEvo
+                </a>
+                <div class="nav-login">
+                    <form action="">
+                        <input type="text" name="uid" placeholder="Username">
+                        <input type="password" name="pwd" placeholder="password">
+                        <button type="submit">Login</button>
+                    </form>
+                    <a href="signup.php">signup</a>
+                </div>
+            </div>
+        </nav>
+    </head>
